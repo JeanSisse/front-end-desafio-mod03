95bde3dc5e212c9eea85eabf1ecc3b2536443fb4
Jean Pierre Sissé <tavares1988@live.com>
2021-11-18 08:04:40 -0300

    Removendo App.js e iniciando estrutura de paginas contendo Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import Header from "../../Components/Header";
+import Main from "../../Components/Main";
+
+function Home() {
+  return (
+    <div className="card-controle">
+      <Header />
+      <Main/>
+    </div>
+  );
+}
+
+export default Home;
